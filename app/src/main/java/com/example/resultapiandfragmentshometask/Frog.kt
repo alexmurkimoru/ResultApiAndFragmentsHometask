4f305199de9dd86d23afd1bc8634ff8edc4d6bf6
alexmurkimoru <sashmanzheeva@gmail.com>
2024-01-29 15:45:33 +0300

    Technically code is working, but everything is dirty AF

---
+++ app/src/main/java/com/example/resultapiandfragmentshometask/Frog.kt
@@ -0,0 +1,8 @@
+package com.example.resultapiandfragmentshometask
+
+data class Frog(
+    val name: String = "testName",
+    val age: Int = 0,
+    val skinId: Int = 0,
+    var hunger: Int = 100,
+    var happiness: Int = 100)
