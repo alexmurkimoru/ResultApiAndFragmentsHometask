299e3024fd4638308bcdaac7103cc71031b652d8
alexmurkimoru <sashmanzheeva@gmail.com>
2024-03-03 16:58:38 +0300

    version before making fragment branch

--- app/src/main/java/com/example/resultapiandfragmentshometask/Frog.kt
+++ app/src/main/java/com/example/resultapiandfragmentshometask/Frog.kt
@@ -1,10 +1,9 @@
 package com.example.resultapiandfragmentshometask
 
-import java.net.IDN
-
+import androidx.annotation.DrawableRes
 data class Frog(
     val name: String = "testName",
-    val skinId: Int = 0,
+    @DrawableRes val skinId: Int = 0,
     var hunger: Int = 3,
     var joy: Int = 3,
     var clear: Int = 3
